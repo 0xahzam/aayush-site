a9faa4fc26d6a38f3b15d9d40d887c12aaf0a3a7
Ahzam Khan <104062587+ahzamkhanx@users.noreply.github.com>
2022-10-02 14:01:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
           Hey, I&apos;m <span className="exag" style={{color:"#D2FFD1", textDecoration:"underline"}}>Aayush</span> —  I write <span className="exag" style={{color:"#FF72B9"}}>smartcontracts.</span> 
         </Text>
         <Text color={"white"}>
-          currently  → <span className="exag" style={{color:"#F5841F"}}>founder's</span> intern <span className="exag" style={{color:"#D2FFD1"}}><a href="https://twitter.com/manangouhari" target={"blank"}>@mglabs</a></span>
+          currently  → <span className="exag" style={{color:"#F5841F"}}>founder&apos;s</span> intern <span className="exag" style={{color:"#D2FFD1"}}><a href="https://twitter.com/manangouhari" target={"blank"}>@mglabs</a></span>
         </Text>
         <Flex backgroundColor={"#0C0C0C"} width={{base:"200px",md:"242px"}} rounded={"7px"} marginTop={"17px"} color={"white"} fontSize={"14px"} justifyContent="center" p={"7px"}>
         <Text>
